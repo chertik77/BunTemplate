0bc8c432d835bc5f2fb65f44656d1e61f790b04e
chertik77 <babychdenya@gmail.com>
2023-10-03 14:24:59 +0100

    add fullreload plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), FullReload(['./**/**.html'])],
   resolve: {
     alias: {
       src: '/src',

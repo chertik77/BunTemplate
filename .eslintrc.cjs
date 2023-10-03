f9deccb9a512d45c7e19c8bd6a4a0fc9cff348d4
chertik77 <babychdenya@gmail.com>
2023-10-03 19:28:44 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,7 +16,7 @@ module.exports = {
     ecmaFeatures: { jsx: true },
   },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', '@typescript-eslint'],
+  plugins: ['react-refresh'],
   rules: { 'react-refresh/only-export-components': ['warn', { allowConstantExport: true }] },
   overrides: [
     {
